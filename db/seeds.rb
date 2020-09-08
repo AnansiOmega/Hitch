e90dfaeb5e4d3c04533a7eb713697bf68ed7ebbb
Brian Butterly <butterlybrian@gmail.com>
2020-09-08 15:13:13 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Delivery.destroy_all
+Reciever.destroy_all
+Supplier.destroy_all
+Driver.destroy_all
+Broker.destroy_all
+
+"Seed that shit"
+
+20.times do
+    Broker.create(name: Faker::FunnyName.name, email: Faker::Internet.email, password_digest: Faker::Internet.password)   
+end
+
+20.times do
+    Driver.create(name: Faker::FunnyName.two_word_name, email: Faker::Internet.email, password_digest: Faker::Internet.password)
+end
+
+20.times do
+    Supplier.create(company: Faker::Company.name, location: Faker::Address.city)
+end
+
+20.times do
+    Reciever.create(company: Faker::Company.name, location: Faker::Address.city)
+end
+
+x = ["8'6", "9'6"]
+y = [Faker::Food.fruits, Faker::Construction.material]
+
+20.times do
+    Delivery.create(broker_id: Broker.all.sample.id, driver_id: Driver.all.sample.id, supplier_id: Supplier.all.sample.id, 
+    reciever_id: Reciever.all.sample.id, hazardous: Faker::Boolean.boolean, pickup_time: rand(1..12), arrival_time: rand(1..12), 
+    dropoff_time: rand(1..12), height: x.sample, weight: "#{rand(6..40)} tons", description: "#{rand(50..5000)} pallets of #{y.sample}")
+end
+
+"donezo"
