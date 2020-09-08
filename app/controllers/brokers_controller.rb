4b0e131f8fc8a8e53210c4ddbb737281deb83c82
AnansiOmega <ignasbutautas7@gmail.com>
2020-09-08 17:24:43 -0500

    created user story for broker

--- app/controllers/brokers_controller.rb
+++ app/controllers/brokers_controller.rb
@@ -1,2 +1,30 @@
 class BrokersController < ApplicationController
+
+    def index
+        @brokers = Broker.all
+    end
+
+    def show
+        @broker = Broker.find(params[:id])
+    end
+
+    def new
+        @broker = Broker.new
+    end
+
+    def create
+        broker = Broker.create(broker_params)
+
+        redirect_to broker_path(broker)
+    end
+
+
+    private
+
+
+    def broker_params
+        params.require(:broker).permit(:name,:email, :password)
+    end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ y = [Faker::Food.fruits, Faker::Construction.material]
 200.times do
     Delivery.create(broker_id: Broker.all.sample.id, driver_id: Driver.all.sample.id, supplier_id: Supplier.all.sample.id, 
     receiver_id: Receiver.all.sample.id, hazardous: Faker::Boolean.boolean, pickup_time: rand(1..12), arrival_time: rand(1..12), 
-    dropoff_time: rand(1..12), height: x.sample, weight: "#{rand(6..40)} tons", description: "#{rand(50..5000)} pallets of #{y.sample}")
+    dropoff_time: rand(1..12), height: x.sample, weight: "#{rand(6..40)} tons", description: "#{rand(50..5000)} pallets of #{y.sample}", status: "Awaiting Pickup")
 end
 
 "donezo"
