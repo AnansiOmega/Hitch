e3a0e9b4912ada0088f7cf7e17409b9001eb8534
AnansiOmega <ignasbutautas7@gmail.com>
2020-09-08 15:23:50 -0500

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 Delivery.destroy_all
-Reciever.destroy_all
+Receiver.destroy_all
 Supplier.destroy_all
 Driver.destroy_all
 Broker.destroy_all
@@ -20,15 +20,15 @@ end
 end
 
 20.times do
-    Reciever.create(company: Faker::Company.name, location: Faker::Address.city)
+    Receiver.create(company: Faker::Company.name, location: Faker::Address.city)
 end
 
 x = ["8'6", "9'6"]
 y = [Faker::Food.fruits, Faker::Construction.material]
 
-20.times do
+200.times do
     Delivery.create(broker_id: Broker.all.sample.id, driver_id: Driver.all.sample.id, supplier_id: Supplier.all.sample.id, 
-    reciever_id: Reciever.all.sample.id, hazardous: Faker::Boolean.boolean, pickup_time: rand(1..12), arrival_time: rand(1..12), 
+    receiver_id: Receiver.all.sample.id, hazardous: Faker::Boolean.boolean, pickup_time: rand(1..12), arrival_time: rand(1..12), 
     dropoff_time: rand(1..12), height: x.sample, weight: "#{rand(6..40)} tons", description: "#{rand(50..5000)} pallets of #{y.sample}")
 end
 
