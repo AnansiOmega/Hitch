5f2e3301b0a727f2c70a34618d96d80800a99f5d
Brian Butterly <butterlybrian@gmail.com>
2020-09-08 17:28:25 -0500

    deliveries show page

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,2 +1,9 @@
 class DeliveriesController < ApplicationController
+    def index
+        @deliveries = Delivery.all
+    end
+
+    def show
+        @delivery = Delivery.find(params[:id])
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
+puts "destroy"
 
 Delivery.destroy_all
-Reciever.destroy_all
+Receiver.destroy_all
 Supplier.destroy_all
 Driver.destroy_all
 Broker.destroy_all
 
-"Seed that shit"
+puts "Seed that shit"
 
 20.times do
     Broker.create(name: Faker::FunnyName.name, email: Faker::Internet.email, password_digest: Faker::Internet.password)   
@@ -20,16 +21,17 @@ end
 end
 
 20.times do
-    Reciever.create(company: Faker::Company.name, location: Faker::Address.city)
+    Receiver.create(company: Faker::Company.name, location: Faker::Address.city)
 end
 
 x = ["8'6", "9'6"]
-y = [Faker::Food.fruits, Faker::Construction.material]
 
-20.times do
+
+200.times do
     Delivery.create(broker_id: Broker.all.sample.id, driver_id: Driver.all.sample.id, supplier_id: Supplier.all.sample.id, 
-    reciever_id: Reciever.all.sample.id, hazardous: Faker::Boolean.boolean, pickup_time: rand(1..12), arrival_time: rand(1..12), 
-    dropoff_time: rand(1..12), height: x.sample, weight: "#{rand(6..40)} tons", description: "#{rand(50..5000)} pallets of #{y.sample}")
+    receiver_id: Receiver.all.sample.id, hazardous: Faker::Boolean.boolean, pickup_time: "#{rand(1..12)} o'clock", arrival_time: "#{rand(1..12)} o'clock", 
+    dropoff_time: "#{rand(1..12)} o'clock", height: x.sample, weight: "#{rand(6..40)} tons", 
+    description: "#{rand(50..5000)} pallets of #{Faker::Construction.material}")
 end
 
-"donezo"
+puts "donezo"
