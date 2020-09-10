e9e60c5590e45fc1056388a39f696327b9af9e64
AnansiOmega <ignasbutautas7@gmail.com>
2020-09-10 14:00:40 -0500

    fleshed out drivers and fixed a few validations

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,6 +1,6 @@
 class DeliveriesController < ApplicationController
     before_action :authorized_broker, :authorized_driver
-    before_action :find_delivery, only: [:show, :destroy, :pickup, :dropoff]
+    before_action :find_delivery, only: [:show, :destroy, :pickup, :dropoff, :transit, :dropped_off]
     skip_before_action :authorized_driver, except: [:pickup, :dropoff]
     skip_before_action :authorized_broker
 
@@ -32,25 +32,21 @@ class DeliveriesController < ApplicationController
     end
 
     def pickup
-        @delivery = Delivery.find(params[:id])
         @delivery.pickup
         redirect_to delivery_transit_path(@delivery)
     end
 
     def dropoff
-        @delivery = Delivery.find(params[:id])
         @delivery.dropoff
         redirect_to delivery_dropped_off_path(@delivery)
     end
 
     def transit
-        delivery = Delivery.find(params[:id])
-        @driver = delivery.driver
+        @driver = @delivery.driver
     end
 
     def dropped_off
-        delivery = Delivery.find(params[:id])
-        @driver = delivery.driver
+        @driver = @delivery.driver
     end
 
     private
